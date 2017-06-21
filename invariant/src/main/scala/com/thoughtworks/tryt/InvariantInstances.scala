6ab735f510c608bd405f7760c5ee31b699bbecb7
Yang Bo <atryyang@thoughtworks.com>
2017-06-21 15:52:07 +0800

    Add `@group` for tryTMonadTrans

--- invariant/src/main/scala/com/thoughtworks/tryt/InvariantInstances.scala
+++ invariant/src/main/scala/com/thoughtworks/tryt/InvariantInstances.scala
@@ -1,15 +1,13 @@
 package com.thoughtworks.tryt
 
 import scala.language.higherKinds
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success, Try}
-import scalaz.Tags.Parallel
-import scalaz.{\/, _}
+import scala.util._
+import scalaz._
 import com.thoughtworks.tryt.invariant.TryT
 
 private[tryt] trait InvariantInstances { this: TryT.type =>
 
-  @inline
+  /** @group Type classes */
   implicit final def tryTMonadTrans: MonadTrans[TryT] = new MonadTrans[TryT] {
     override def liftM[G[_], A](a: G[A])(implicit monad: Monad[G]): TryT[G, A] = {
       TryT(monad.map(a)(Success(_)))
