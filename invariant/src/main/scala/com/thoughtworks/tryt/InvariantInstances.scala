5a34cd03acd2ac8586be1d4f5b1408c0b9a5ab02
Yang Bo <atryyang@thoughtworks.com>
2017-06-21 17:30:42 +0800

    Fix compilation error

--- invariant/src/main/scala/com/thoughtworks/tryt/InvariantInstances.scala
+++ invariant/src/main/scala/com/thoughtworks/tryt/InvariantInstances.scala
@@ -1,7 +1,6 @@
 package com.thoughtworks.tryt
 
 import scala.language.higherKinds
-import scala.util._
 import scalaz._
 import com.thoughtworks.tryt.invariant.TryT
 
@@ -10,7 +9,7 @@ private[tryt] trait InvariantInstances { this: TryT.type =>
   /** @group Type classes */
   implicit final def tryTMonadTrans: MonadTrans[TryT] = new MonadTrans[TryT] {
     override def liftM[G[_], A](a: G[A])(implicit monad: Monad[G]): TryT[G, A] = {
-      TryT(monad.map(a)(Success(_)))
+      TryT(monad.map(a)(scala.util.Success(_)))
     }
 
     override implicit def apply[G[_]: Monad]: Monad[TryT[G, ?]] = tryTMonadError[G]
