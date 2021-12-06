fb5d8bd41a2ed7aaae75375a8b8da04369eff3ea
Scala Steward <me@scala-steward.org>
2021-12-06 08:46:12 +0100

    Reformat with scalafmt 3.1.2

--- covariant/src/main/scala/com/thoughtworks/tryt/covariant.scala
+++ covariant/src/main/scala/com/thoughtworks/tryt/covariant.scala
@@ -11,37 +11,38 @@ import scalaz.{\/, _}
 object covariant {
 
   private[tryt] trait OpacityTypes {
-    type TryT[F[+ _], +A]
+    type TryT[F[+_], +A]
 
-    def apply[F[+ _], A](run: F[Try[A]]): TryT[F, A]
-    def unwrap[F[+ _], A](tryT: TryT[F, A]): F[Try[A]]
+    def apply[F[+_], A](run: F[Try[A]]): TryT[F, A]
+    def unwrap[F[+_], A](tryT: TryT[F, A]): F[Try[A]]
 
   }
 
   @inline
   private[tryt] val opacityTypes: OpacityTypes = new OpacityTypes {
 
-    type TryT[F[+ _], +A] = F[Try[A]]
+    type TryT[F[+_], +A] = F[Try[A]]
 
-    override final def apply[F[+ _], A](tryT: F[Try[A]]): TryT[F, A] = tryT
+    override final def apply[F[+_], A](tryT: F[Try[A]]): TryT[F, A] = tryT
 
-    override final def unwrap[F[+ _], A](tryT: TryT[F, A]): F[Try[A]] = tryT
+    override final def unwrap[F[+_], A](tryT: TryT[F, A]): F[Try[A]] = tryT
 
   }
 
   object TryT extends TryTInstances0 {
-    private[thoughtworks] def unwrap[F[+ _], A](tryT: TryT[F, A]): F[Try[A]] = opacityTypes.unwrap(tryT)
+    private[thoughtworks] def unwrap[F[+_], A](tryT: TryT[F, A]): F[Try[A]] = opacityTypes.unwrap(tryT)
 
     /** @group Converters */
-    def unapply[F[+ _], A](tryT: TryT[F, A]): Some[F[Try[A]]] = Some(unwrap(tryT))
+    def unapply[F[+_], A](tryT: TryT[F, A]): Some[F[Try[A]]] = Some(unwrap(tryT))
 
     /** @group Converters */
-    def apply[F[+ _], A](tryT: F[Try[A]]): TryT[F, A] = opacityTypes.apply(tryT)
+    def apply[F[+_], A](tryT: F[Try[A]]): TryT[F, A] = opacityTypes.apply(tryT)
 
     /** @group Type classes */
-    implicit final def tryTMonadCatchIORec[F[+ _]](
-        implicit F0: MonadIO[F],
-        B0: BindRec[F]): MonadCatchIO[TryT[F, _]] with BindRec[TryT[F, _]] = {
+    implicit final def tryTMonadCatchIORec[F[+_]](implicit
+        F0: MonadIO[F],
+        B0: BindRec[F]
+    ): MonadCatchIO[TryT[F, _]] with BindRec[TryT[F, _]] = {
       new MonadCatchIO[TryT[F, _]] with TryTBindRec[F] with TryTLiftIO[F] with TryTMonadError[F] {
         override implicit def B: BindRec[F] = B0
         override implicit def F: MonadIO[F] = F0
@@ -52,16 +53,17 @@ object covariant {
   private[tryt] sealed abstract class TryTInstances3 { this: TryT.type =>
 
     /** @group Type classes */
-    implicit final def tryTMonadCatchIO[F[+ _]](implicit F0: MonadIO[F]): MonadCatchIO[TryT[F, _]] = {
+    implicit final def tryTMonadCatchIO[F[+_]](implicit F0: MonadIO[F]): MonadCatchIO[TryT[F, _]] = {
       new MonadCatchIO[TryT[F, _]] with TryTLiftIO[F] with TryTMonadError[F] {
         override implicit def F: MonadIO[F] = F0
       }
     }
 
     /** @group Type classes */
-    implicit final def tryTParallelApplicative[F[+ _]](
-        implicit F0: Applicative[λ[A => F[A] @@ Parallel]],
-        S0: Semigroup[Throwable]): Applicative[λ[A => TryT[F, A] @@ Parallel]] = {
+    implicit final def tryTParallelApplicative[F[+_]](implicit
+        F0: Applicative[λ[A => F[A] @@ Parallel]],
+        S0: Semigroup[Throwable]
+    ): Applicative[λ[A => TryT[F, A] @@ Parallel]] = {
       new TryTParallelApplicative[F] {
         override implicit def F: Applicative[λ[A => F[A] @@ Parallel]] = F0
         override implicit def S: Semigroup[Throwable] = S0
@@ -72,9 +74,10 @@ object covariant {
   private[tryt] sealed abstract class TryTInstances2 extends TryTInstances3 { this: TryT.type =>
 
     /** @group Type classes */
-    implicit final def tryTBindRec[F[+ _]](
-        implicit F0: Monad[F],
-        B0: BindRec[F]): BindRec[TryT[F, _]] with MonadError[TryT[F, _], Throwable] = {
+    implicit final def tryTBindRec[F[+_]](implicit
+        F0: Monad[F],
+        B0: BindRec[F]
+    ): BindRec[TryT[F, _]] with MonadError[TryT[F, _], Throwable] = {
       new TryTBindRec[F] with TryTMonadError[F] {
         override implicit def B: BindRec[F] = B0
         override implicit def F: Monad[F] = F0
@@ -85,7 +88,7 @@ object covariant {
   private[tryt] sealed abstract class TryTInstances1 extends TryTInstances2 { this: TryT.type =>
 
     /** @group Type classes */
-    implicit final def tryTMonadError[F[+ _]](implicit F0: Monad[F]): MonadError[TryT[F, _], Throwable] = {
+    implicit final def tryTMonadError[F[+_]](implicit F0: Monad[F]): MonadError[TryT[F, _], Throwable] = {
       new TryTMonadError[F] {
         implicit override def F: Monad[F] = F0
       }
@@ -95,13 +98,13 @@ object covariant {
   private[tryt] sealed abstract class TryTInstances0 extends TryTInstances1 { this: TryT.type =>
 
     /** @group Type classes */
-    implicit final def tryTLiftIO[F[+ _]](implicit F0: LiftIO[F]): LiftIO[TryT[F, _]] =
+    implicit final def tryTLiftIO[F[+_]](implicit F0: LiftIO[F]): LiftIO[TryT[F, _]] =
       new TryTLiftIO[F] {
         implicit override def F: LiftIO[F] = F0
       }
 
     /** @group Type classes */
-    implicit final def tryTFunctor[F[+ _]](implicit F0: Functor[F]): Functor[TryT[F, _]] =
+    implicit final def tryTFunctor[F[+_]](implicit F0: Functor[F]): Functor[TryT[F, _]] =
       new TryTFunctor[F] {
         implicit override def F: Functor[F] = F0
       }
@@ -109,7 +112,7 @@ object covariant {
 
   import opacityTypes._
 
-  private[tryt] trait TryTFunctor[F[+ _]] extends Functor[TryT[F, _]] {
+  private[tryt] trait TryTFunctor[F[+_]] extends Functor[TryT[F, _]] {
     implicit protected def F: Functor[F]
 
     override def map[A, B](fa: TryT[F, A])(f: A => B): TryT[F, B] = {
@@ -121,7 +124,7 @@ object covariant {
     }
   }
 
-  private[tryt] trait TryTBind[F[+ _]] extends Bind[TryT[F, _]] with TryTFunctor[F] {
+  private[tryt] trait TryTBind[F[+_]] extends Bind[TryT[F, _]] with TryTFunctor[F] {
     implicit protected override def F: Monad[F]
 
     override def bind[A, B](fa: TryT[F, A])(f: A => TryT[F, B]): TryT[F, B] = opacityTypes {
@@ -140,7 +143,7 @@ object covariant {
     }
   }
 
-  private[tryt] trait TryTBindRec[F[+ _]] extends BindRec[TryT[F, _]] with TryTBind[F] {
+  private[tryt] trait TryTBindRec[F[+_]] extends BindRec[TryT[F, _]] with TryTBind[F] {
     implicit protected def F: Monad[F]
     implicit protected def B: BindRec[F]
 
@@ -159,12 +162,13 @@ object covariant {
             }
           case Failure(e) =>
             F.point(\/-(Failure(e)))
-      })
+        }
+      )
       opacityTypes(fTryB)
     }
   }
 
-  private[tryt] trait TryTLiftIO[F[+ _]] extends LiftIO[TryT[F, _]] {
+  private[tryt] trait TryTLiftIO[F[+_]] extends LiftIO[TryT[F, _]] {
     implicit protected def F: LiftIO[F]
 
     override def liftIO[A](ioa: IO[A]): TryT[F, A] = {
@@ -173,7 +177,7 @@ object covariant {
 
   }
 
-  private[tryt] trait TryTMonadError[F[+ _]] extends MonadError[TryT[F, _], Throwable] with TryTBind[F] {
+  private[tryt] trait TryTMonadError[F[+_]] extends MonadError[TryT[F, _], Throwable] with TryTBind[F] {
     implicit protected override def F: Monad[F]
 
     override def point[A](a: => A): TryT[F, A] = opacityTypes.apply(F.point(Try(a)))
@@ -199,7 +203,7 @@ object covariant {
     }
   }
 
-  private[tryt] trait TryTParallelApplicative[F[+ _]] extends Applicative[λ[A => TryT[F, A] @@ Parallel]] {
+  private[tryt] trait TryTParallelApplicative[F[+_]] extends Applicative[λ[A => TryT[F, A] @@ Parallel]] {
     implicit protected def F: Applicative[λ[A => F[A] @@ Parallel]]
     implicit protected def S: Semigroup[Throwable]
     private type T[+A] = TryT[F, A]
@@ -220,19 +224,21 @@ object covariant {
 
     override def ap[A, B](fa: => P[A])(f: => P[(A) => B]): P[B] = {
 
-      val fTryAP: F[Try[A]] @@ Parallel = try {
-        Parallel(opacityTypes.unwrap(Parallel.unwrap(fa)))
-      } catch {
-        case NonFatal(e) =>
-          F.point(Failure(e))
-      }
+      val fTryAP: F[Try[A]] @@ Parallel =
+        try {
+          Parallel(opacityTypes.unwrap(Parallel.unwrap(fa)))
+        } catch {
+          case NonFatal(e) =>
+            F.point(Failure(e))
+        }
 
-      val fTryABP: F[Try[A => B]] @@ Parallel = try {
-        Parallel(opacityTypes.unwrap(Parallel.unwrap(f)): F[Try[A => B]])
-      } catch {
-        case NonFatal(e) =>
-          F.point(Failure(e))
-      }
+      val fTryABP: F[Try[A => B]] @@ Parallel =
+        try {
+          Parallel(opacityTypes.unwrap(Parallel.unwrap(f)): F[Try[A => B]])
+        } catch {
+          case NonFatal(e) =>
+            F.point(Failure(e))
+        }
 
       import scalaz.syntax.all._
 
@@ -263,77 +269,78 @@ object covariant {
 
   /** A monad transformer for exception handling.
     *
-    * @see This `TryT` transfomer is similar to [[scalaz.EitherT]],
-    *      except `TryT` handles exceptions thrown in callback functions passed to
-    *      [[scalaz.Monad.map map]], [[scalaz.Monad.bind bind]] or [[scalaz.Monad.point point]].
+    * @see
+    *   This `TryT` transfomer is similar to [[scalaz.EitherT]], except `TryT` handles exceptions thrown in callback
+    *   functions passed to [[scalaz.Monad.map map]], [[scalaz.Monad.bind bind]] or [[scalaz.Monad.point point]].
     *
-    * @example As a monad transformer, `TryT` should be used with another monadic data type, like [[scalaz.Name]].
+    * @example
+    *   As a monad transformer, `TryT` should be used with another monadic data type, like [[scalaz.Name]].
     *
-    *          {{{
-    *          import scalaz.Name
-    *          import com.thoughtworks.tryt.covariant.TryT, TryT._
+    * {{{
+    *           import scalaz.Name
+    *           import com.thoughtworks.tryt.covariant.TryT, TryT._
     *
-    *          type TryName[+A] = TryT[Name, A]
-    *          }}}
+    *           type TryName[+A] = TryT[Name, A]
+    * }}}
     *
-    *          Given a `validate` function,
+    * Given a `validate` function,
     *
-    *          {{{
-    *          def validate(s: String): Int = s.toInt
-    *          }}}
+    * {{{
+    *           def validate(s: String): Int = s.toInt
+    * }}}
     *
-    *          when creating a `TryT`-transformed [[scalaz.Name]] from the `validate`,
+    * when creating a `TryT`-transformed [[scalaz.Name]] from the `validate`,
     *
-    *          {{{
-    *          import scalaz.syntax.all._
-    *          val invalidTry: TryName[Int] = validate("invalid input").point[TryName]
-    *          }}}
+    * {{{
+    *           import scalaz.syntax.all._
+    *           val invalidTry: TryName[Int] = validate("invalid input").point[TryName]
+    * }}}
     *
-    *          then the exceptions thrown in `validate` call should be converted to a [[scala.util.Failure]];
+    * then the exceptions thrown in `validate` call should be converted to a [[scala.util.Failure]];
     *
-    *          {{{
-    *          import com.thoughtworks.tryt.covariant.TryT._
+    * {{{
+    *           import com.thoughtworks.tryt.covariant.TryT._
     *
-    *          val TryT(Name(failure)) = invalidTry
+    *           val TryT(Name(failure)) = invalidTry
     *
-    *          import scala.util._
-    *          failure should be(an[Failure[_]])
-    *          }}}
+    *           import scala.util._
+    *           failure should be(an[Failure[_]])
+    * }}}
     *
-    *          and when there is no exception thrown in `validate` call,
+    * and when there is no exception thrown in `validate` call,
     *
-    *          {{{
-    *          val validTry: TryName[Int] = validate("42").point[TryName]
-    *          }}}
+    * {{{
+    *           val validTry: TryName[Int] = validate("42").point[TryName]
+    * }}}
     *
-    *          then the result of `validate` call should be converted to a [[scala.util.Success]];
+    * then the result of `validate` call should be converted to a [[scala.util.Success]];
     *
-    *          {{{
-    *          val TryT(Name(success)) = validTry
-    *          success should be(Success(42))
-    *          }}}
+    * {{{
+    *           val TryT(Name(success)) = validTry
+    *           success should be(Success(42))
+    * }}}
     *
-    *          and when the `TryT`-transformed [[scalaz.Name]] is built from a `for`-comprehension,
+    * and when the `TryT`-transformed [[scalaz.Name]] is built from a `for`-comprehension,
     *
-    *          {{{
-    *          val invalidForComprehension: TryName[Int] = for {
-    *            i <- validate("42").point[TryName]
-    *            j <- validate("invalid input").point[TryName]
-    *          } yield i + j
-    *          }}}
+    * {{{
+    *           val invalidForComprehension: TryName[Int] = for {
+    *             i <- validate("42").point[TryName]
+    *             j <- validate("invalid input").point[TryName]
+    *           } yield i + j
+    * }}}
     *
-    *          then the exceptions thrown in the `for`-comprehension should be converted to a [[scala.util.Failure]];
+    * then the exceptions thrown in the `for`-comprehension should be converted to a [[scala.util.Failure]];
     *
-    *          {{{
-    *          val TryT(Name(failure2)) = invalidTry
-    *          failure2 should be(an[Failure[_]])
-    *          }}}
+    * {{{
+    *           val TryT(Name(failure2)) = invalidTry
+    *           failure2 should be(an[Failure[_]])
+    * }}}
     *
-    *
-    * @note This `TryT` type is an opacity alias to `F[Try[A]]`.
-    *       All type classes and helper functions for this `TryT` type are defined in the companion object [[TryT$ TryT]]
+    * @note
+    *   This `TryT` type is an opacity alias to `F[Try[A]]`. All type classes and helper functions for this `TryT` type
+    *   are defined in the companion object [[TryT$ TryT]]
     * @template
     */
-  type TryT[F[+ _], +A] = opacityTypes.TryT[F, A]
+  type TryT[F[+_], +A] = opacityTypes.TryT[F, A]
 
 }
